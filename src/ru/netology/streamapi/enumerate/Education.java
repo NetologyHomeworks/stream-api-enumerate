27f925e5e6c13ffe86c91a4a34194acae4a6f9eb
Verg82 <verg82@mail.ru>
2022-05-24 21:34:38 +0800

    Initial commit

---
+++ src/ru/netology/streamapi/enumerate/Education.java
@@ -0,0 +1,8 @@
+package ru.netology.streamapi.enumerate;
+
+public enum Education {
+    ELEMENTARY,
+    SECONDARY,
+    FURTHER,
+    HIGHER
+}
---
+++ src/ru/netology/streamapi/enumerate/Sex.java
@@ -0,0 +1,6 @@
+package ru.netology.streamapi.enumerate;
+
+public enum Sex {
+    MAN,
+    WOMAN
+}
